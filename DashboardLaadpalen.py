6a30984bcc71662b679ce76f519f2586e9dc284d
rhoderebel <91129594+rhoderebel@users.noreply.github.com>
2021-10-13 21:29:51 +0200

    Update DashboardLaadpalen.py

--- DashboardLaadpalen.py
+++ DashboardLaadpalen.py
@@ -22,7 +22,7 @@ from streamlit_folium import folium_static
 st.set_page_config(page_title = 'Dashboard Case 3', layout = 'wide')
 st.title("Dashboard elektrische auto's, laadstations en laadpaalgebruik")
 st.markdown('Vincent Kemme (500838439), Rhodé Rebel (500819128), Amber van der Pol (500803136) en Oussama Abou (500803060)')
-st.markdown('''Dit interactieve dashboard geeft weer hoe de geïnspecteerde data verdeeld is. Het dashboard geeft bijvoorbeeld inzichten over hoe de brandstoftypes van auto’s verdeeld zijn, hoe laadstations verdeeld zijn over Nederland en hoe laadtijden per sessie verdeeld zijn.''')
+st.info('''Dit interactieve dashboard geeft weer hoe de geïnspecteerde data verdeeld is. Het dashboard geeft bijvoorbeeld inzichten over hoe de brandstoftypes van auto’s verdeeld zijn, hoe laadstations verdeeld zijn over Nederland en hoe laadtijden per sessie verdeeld zijn.''')
 st.header("Databronnen")
 st.markdown('''De data die gebruikt wordt voor dit dashboard komt van drie verschillende bronnen.  
 
