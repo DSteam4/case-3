08eef113e8a67f04f1dc47a00b58e71e73eace80
rhoderebel <91129594+rhoderebel@users.noreply.github.com>
2021-10-13 16:19:15 +0200

    Update DashboardLaadpalen.py

--- DashboardLaadpalen.py
+++ DashboardLaadpalen.py
@@ -464,4 +464,4 @@ operationtype_leg = add_categorical_legend(m_dual, 'Werking laadstation', colors
 
 folium.LayerControl(position = 'topleft').add_to(m_dual)
 
-folium_static(m_dual, width = 800)
+folium_static(m_dual, width = 1500)
