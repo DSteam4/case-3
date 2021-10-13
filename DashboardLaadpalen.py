58eeabd4eed0ee640698eb74ad1ae39ff800ea26
rhoderebel <91129594+rhoderebel@users.noreply.github.com>
2021-10-13 22:33:31 +0200

    Update DashboardLaadpalen.py

--- DashboardLaadpalen.py
+++ DashboardLaadpalen.py
@@ -582,8 +582,6 @@ def regmodel(Y, X):
 regmodel(dflpdpos['TotalEnergy'],dflpdpos[['ConnectedTime','ChargeTime']])
 
 g1 = sns.regplot(x = dflpdpos['TotalEnergy'], y = dflpdpos['ConnectedTime'])
-st.pyplot(g1)
 
 g2 = sns.regplot(x = dflpdpos['TotalEnergy'], y = dflpdpos['ChargeTime'])
 g2.set(xlim=(0,80000),ylim=(0,24))
-st.pyplot(g2)
