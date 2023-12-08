2ea6eb2ec73afde0c14845f82550668559bf7150
rhoderebel <91129594+rhoderebel@users.noreply.github.com>
2023-12-08 09:22:39 +0100

    Update DashboardLaadpalen.py

--- DashboardLaadpalen.py
+++ DashboardLaadpalen.py
@@ -19,7 +19,7 @@ import streamlit_folium
 import rtree
 from streamlit_folium import folium_static
 import plotly.figure_factory as ff
-import statsmodels.api as sm
+import statsmodels as sm
 import matplotlib.pyplot as plt
 
 st.set_page_config(page_title = 'Dashboard Case 3', layout = 'wide')
